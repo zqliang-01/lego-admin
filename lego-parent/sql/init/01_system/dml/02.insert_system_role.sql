5511e329f21aade47b4ffeb000f5b296b04511b2
zqliang <zqliang_01@163.com>
2024-07-21 18:22:00 +0800

    数据权限初始化脚本调整

--- lego-parent/sql/init/01_system/dml/02.insert_system_role.sql
+++ lego-parent/sql/init/01_system/dml/02.insert_system_role.sql
@@ -1,5 +1,5 @@
 INSERT INTO sys_role(ID, CODE, NAME, VERSION, CREATE_TIME, DELETED, DATA_SCOPE)
-VALUES (next_id('general'), 'admin', '超级管理员', 1, sysdate(), 0， 3);
+VALUES (next_id('general'), 'admin', '超级管理员', 1, sysdate(), 0, 3);
 
 INSERT INTO sys_role(ID, CODE, NAME, VERSION, CREATE_TIME, DELETED, DATA_SCOPE)
 VALUES (next_id('general'), 'TEST001', '体验角色', 1, sysdate(), 0, 3);
