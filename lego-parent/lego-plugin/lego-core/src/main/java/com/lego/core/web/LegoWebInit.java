dedb4169a92a26e463eca220b86b456b7ef7c183
zqliang <zqliang_01@163.com>
2024-10-29 15:38:52 +0800

    系统simsun字体库自安装异常捕获

--- lego-parent/lego-plugin/lego-core/src/main/java/com/lego/core/web/LegoWebInit.java
+++ lego-parent/lego-plugin/lego-core/src/main/java/com/lego/core/web/LegoWebInit.java
@@ -1,6 +1,6 @@
 package com.lego.core.web;
 
-import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Component;
 import java.awt.*;
 import java.io.InputStream;
 
+@Slf4j
 @Component
 public class LegoWebInit implements BeanFactoryPostProcessor {
 
@@ -17,11 +18,13 @@ public class LegoWebInit implements BeanFactoryPostProcessor {
         initFont();
     }
 
-    @SneakyThrows
     private void initFont() {
-        InputStream fontStream = this.getClass().getResourceAsStream("/fonts/simsun.ttf");
-        Font font = Font.createFont(Font.TRUETYPE_FONT, fontStream);
-        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-        ge.registerFont(font);
+        try (InputStream fontStream = this.getClass().getResourceAsStream("/fonts/simsun.ttf")) {
+            Font font = Font.createFont(Font.TRUETYPE_FONT, fontStream);
+            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            ge.registerFont(font);
+        } catch (Exception e) {
+            log.error("字体库/fonts/simsun.ttf安装失败，可能会影响到部分字体显示乱码", e);
+        }
     }
 }
