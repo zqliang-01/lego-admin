be7c0b014b8638caa3fc6495a58d7135bf8d7bf3
zqliang <zqliang_01@163.com>
2025-01-21 14:52:25 +0800

    修复日志管理中过滤用户条件无效的问题

--- lego-parent/lego-plugin/lego-system/src/main/java/com/lego/system/dao/impl/SysOperationLogDao.java
+++ lego-parent/lego-plugin/lego-system/src/main/java/com/lego/system/dao/impl/SysOperationLogDao.java
@@ -13,36 +13,36 @@ import java.util.List;
 
 public class SysOperationLogDao extends GenericDao<SysOperationLog> implements ISysOperationLogDao {
 
-	@Override
-	public List<SysOperationLog> findBy(String loginCode, String entityCode, String permissionCode) {
-		QueryHandler<SysOperationLog> query = createQueryHandler();
-		query.condition("t.entityCode = :entityCode").param("entityCode", entityCode);
+    @Override
+    public List<SysOperationLog> findBy(String loginCode, String entityCode, String permissionCode) {
+        QueryHandler<SysOperationLog> query = createQueryHandler();
+        query.condition("t.entityCode = :entityCode").param("entityCode", entityCode);
 //		query.condition("t.operator.code = :operatorCode").param("operatorCode", loginCode);
-		query.condition("t.permission.code = :permissionCode").param("permissionCode", permissionCode);
-		query.order("t.createTime DESC");
-		return query.findList();
-	}
+        query.condition("t.permission.code = :permissionCode").param("permissionCode", permissionCode);
+        query.order("t.createTime DESC");
+        return query.findList();
+    }
 
-	@Override
-	public LegoPage<SysOperationLog> findPageBy(String loginCode, SysOperationLogSearchVO vo) {
-		QueryHandler<SysOperationLog> query = createQueryHandler();
-		if (StringUtil.isNotBlank(vo.getDescription())) {
-			query.condition("t.description LIKE :description").param("description", "%" + vo.getDescription() + "%");
-		}
-		if (!Constants.ADMIN_EMPLOYEE_CODE.equals(loginCode)) {
-			query.condition("t.operator.code <> :operator").param("operator", Constants.ADMIN_EMPLOYEE_CODE);
-		}
-		if (StringUtil.isNotBlank(vo.getOperatorCode())) {
-			query.condition("t.operator.code = :operator").param("operator", vo.getOperatorCode());
-		}
-		if (vo.getStartTime() != null) {
-			query.condition("t.createTime >= :createTime").param("createTime", vo.getStartTime());
-		}
-		if (vo.getEndTime() != null) {
-			query.condition("t.createTime <= :createTime1").param("createTime1", vo.getEndTime());
-		}
-		query.order("t.createTime DESC");
-		return query.findPage(vo);
-	}
+    @Override
+    public LegoPage<SysOperationLog> findPageBy(String loginCode, SysOperationLogSearchVO vo) {
+        QueryHandler<SysOperationLog> query = createQueryHandler();
+        if (StringUtil.isNotBlank(vo.getDescription())) {
+            query.condition("t.description LIKE :description").param("description", "%" + vo.getDescription() + "%");
+        }
+        if (!Constants.ADMIN_EMPLOYEE_CODE.equals(loginCode)) {
+            query.condition("t.operator.code <> :admin").param("admin", Constants.ADMIN_EMPLOYEE_CODE);
+        }
+        if (StringUtil.isNotBlank(vo.getOperatorCode())) {
+            query.condition("t.operator.code = :operator").param("operator", vo.getOperatorCode());
+        }
+        if (vo.getStartTime() != null) {
+            query.condition("t.createTime >= :createTime").param("createTime", vo.getStartTime());
+        }
+        if (vo.getEndTime() != null) {
+            query.condition("t.createTime <= :createTime1").param("createTime1", vo.getEndTime());
+        }
+        query.order("t.createTime DESC");
+        return query.findPage(vo);
+    }
 
 }
