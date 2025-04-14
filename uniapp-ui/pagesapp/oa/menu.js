c76e1ff7bbdf5f716f5a851db34358d98edac7ba
zqliang <zqliang@hugetech.com.cn>
2025-04-14 10:15:32 +0800

    移动端审批应用待办任务标题调整

--- uniapp-ui/pagesapp/oa/menu.js
+++ uniapp-ui/pagesapp/oa/menu.js
@@ -5,7 +5,7 @@ module.exports = {
 		list: [
 			{ code: 'oa_start', name: '发起审批', icon: 'top', path: 'pagesapp/oa/start/index' },
 			{ code: 'oa_owner', name: '我的流程', icon: 'my-task', path: 'pagesapp/oa/owner/index' },
-			{ code: 'oa_undo', name: '待办任务', icon: 'contract', path: 'pagesapp/oa/undo/index' },,
+			{ code: 'oa_undo', name: '待办任务', icon: 'contract', path: 'pagesapp/oa/undo/index' },
 			{ code: 'oa_unclaimed', name: '待签任务', icon: 'icon-related-tasks', path: 'pagesapp/oa/claim/index' },
 			{ code: 'oa_finished', name: '已办任务', icon: 'icon-task-state', path: 'pagesapp/oa/finished/index' }
 		]
